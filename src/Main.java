666cf7b5353d510ec0a5fb52bfedea2ce9be53bd
Evgopka <AlfaBrodyaga@yandex.ru>
2023-09-07 12:01:47 +0300

    n

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
     public static void main (String [] ergs){
         System.out.println(containers(3, 4, 2));
+        System.out.println(containers(5, 0, 2));
         System.out.println(containers(5, 0, 2));}
     public static float containers (int x, int y, int z){
         return x * 20 + y * 50 + z * 100;
     }
 }
-/*
-fdesd.bhjsdfv*/
