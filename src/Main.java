a3fffd4004928ff06c315e290623046c7ea1436e
Evgopka <AlfaBrodyaga@yandex.ru>
2023-09-07 12:37:00 +0300

    123

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         System.out.println(containers(3, 4, 2));
         System.out.println(containers(5, 0, 2));}
     public static float containers (int x, int y, int z){
-        return x * 20 + y * 50 + z * 100;
+        return x * 20 + y * 50 + z * 10;
     }
 }
 /*
